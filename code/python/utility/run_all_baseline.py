0e3c0c28130bb128b77344774b8eeabf9f182976
Hang Yan <yanhangxiong@gmail.com>
2018-09-28 17:09:24 -0500

    Move all jupyter notebook into a folder. Add visualize_magnetometer.ipynb

--- code/python/utility/run_all_baseline.py
+++ code/python/utility/run_all_baseline.py
@@ -11,14 +11,14 @@ sys.path.append('/Users/yanhang/Documents/research/IMUProject/code/python')
 parser = argparse.ArgumentParser()
 parser.add_argument('list', type=str)
 parser.add_argument('--recompute', action='store_true')
-parser.add_argument('--start_length', type=int, default=2500)
+parser.add_argument('--start_length', type=int, default=-1)
 parser.add_argument('--stride', type=float, default=0.67)
 args = parser.parse_args()
 
 exec_path = '../../cpp/cmake-build-relwithdebinfo/imu_localization/IMULocalization_cli'
 model_path = '../../../models/svr_cascade0309'
-preset_list = ['mag_only', 'ori_only', 'full', 'raw']
-# preset_list = ['full']
+# preset_list = ['mag_only', 'ori_only', 'full', 'raw']
+preset_list = ['full']
 
 root_dir = os.path.dirname(args.list)
 data_list = []
@@ -34,12 +34,12 @@ classes = {'handheld', 'leg', 'bag', 'body'}
 all_good = True
 for data in data_list:
     data_path = root_dir + '/' + data[0]
-    if len(data) < 2:
-        print(data_path + ' does not contain placement information.')
-        all_good = False
-    if data[1] not in classes:
-        print(data_path + ' has unknown placement: ' + data[1])
-        all_good = False
+    # if len(data) < 2:
+    #     print(data_path + ' does not contain placement information.')
+    #     all_good = False
+    # if data[1] not in classes:
+    #     print(data_path + ' has unknown placement: ' + data[1])
+    #     all_good = False
     if not os.path.isdir(data_path):
         print(data_path + ' does not exist')
         all_good = False
@@ -52,6 +52,7 @@ for data in data_list:
     if not os.path.isdir(data_path):
         warnings.warn(data_path + ' does not exist. Skip.')
         continue
+
     for preset in preset_list:
         command = "%s %s --model_path %s --preset %s --start_portion_length %d" % (exec_path, data_path, model_path,
                                                                                 preset, args.start_length)
@@ -59,12 +60,12 @@ for data in data_list:
         subprocess.call(command, shell=True)
 
     # Step counting
-    command = 'python3 ../step_counting/enhanced_step_counting.py %s --placement %s --stride %f --start_length %d'\
-              % (data_path, data[1], args.stride, args.start_length)
-    print(command)
-    subprocess.call(command, shell=True)
+    # command = 'python3 ../step_counting/enhanced_step_counting.py %s --placement %s --stride %f --start_length %d'\
+    #           % (data_path, data[1], args.stride, args.start_length)
+    # print(command)
+    # subprocess.call(command, shell=True)
 
-    command = 'python3 ../step_counting/frequency_step_counting.py %s --stride %f --start_length %d'\
-              % (data_path, args.stride, args.start_length)
-    print(command)
-    subprocess.call(command, shell=True)
+    # command = 'python3 ../step_counting/frequency_step_counting.py %s --stride %f --start_length %d'\
+    #           % (data_path, args.stride, args.start_length)
+    # print(command)
+    # subprocess.call(command, shell=True)
