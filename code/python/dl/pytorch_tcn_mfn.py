432d21fb6cbbbe239b5552245e406715e450fb0e
Hang Yan <yanhangxiong@gmail.com>
2018-10-06 21:08:19 -0500

    Refactor the code Replace the target with sin/cos values

--- code/python/dl/pytorch_tcn_mfn.py
+++ code/python/dl/pytorch_tcn_mfn.py
@@ -2,6 +2,7 @@ import os
 from os import path as osp
 import math
 import sys
+import json
 from os import path as osp
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter1d
@@ -27,7 +28,9 @@ _feature_column = ['gyro_x', 'gyro_y', 'gyro_z', 'linacce_x', 'linacce_y', 'lina
 _nano_to_sec = 1e09
 
 _input_channel, _output_channel = 12, 1
-_layer_channels = [24, 48, 96, 192, 192, 192, 192, 96, 48, 24]
+# _layer_channels = [24, 48, 96, 192, 192, 192, 192, 96, 48, 24]
+_layer_channels = [24, 48, 96, 96, 48]
+
 
 def compute_yaw_in_tango(orientations, filter_sigma=None):
     yaw = np.empty(orientations.shape[0])
@@ -48,13 +51,12 @@ def compute_magnet_yaw_in_tango(orientations, magnet, inlier_threshold=0.1):
     return np.mean(inlier)
 
 
-def load_datalist(path):
-    with open(path) as f:
-        data_list = [s.strip().split(',')[0] for s in f.readlines() if s[0] != '#']
-
+def load_datalist(root_dir, data_list):
     train_features = []
     train_targets = []
-    root_dir = osp.dirname(path)
+    feature_smooth = 2.0
+    target_smooth = 10.0
+
     for data in data_list:
         print('Loading ', data)
         data_all = pandas.read_csv(osp.join(root_dir, data, 'processed/data.csv'))
@@ -79,18 +81,22 @@ def load_datalist(path):
 
         yaw_in_tango = compute_yaw_in_tango(orientations)
         north_in_tango = compute_magnet_yaw_in_tango(orientations, magnet)
-        gt_yaw = np.mod(yaw_in_tango - yaw_in_tango[0] + north_in_tango, 2 * math.pi)[:, np.newaxis]
-        feature = gaussian_filter1d(data_all[_feature_column].values, sigma=2.0, axis=0)
+        gt_yaw = np.mod(yaw_in_tango - north_in_tango, 2 * math.pi)[:, np.newaxis]
+
+        feature = gaussian_filter1d(data_all[_feature_column].values, sigma=feature_smooth, axis=0)
+        target = np.concatenate([np.sin(gt_yaw), np.cos(gt_yaw)], axis=1)
+        target = gaussian_filter1d(target, sigma=target_smooth, axis=0)
         train_features.append(feature)
-        train_targets.append(gt_yaw)
+        # train_targets.append(gt_yaw)
+        train_targets.append(target)
 
     return train_features, train_targets
 
 
 class AverageMeter:
-    def __init__(self):
+    def __init__(self, ndim):
         self.count = 0
-        self.sum = 0.0
+        self.sum = np.zeros(ndim)
 
     def add(self, val):
         self.sum += val
@@ -98,13 +104,17 @@ class AverageMeter:
 
     def get_average(self):
         if self == 0:
-            return 0
+            return self.sum
         return self.sum / self.count
 
 
 class IMUMagnetDataset(Dataset):
-    def __init__(self, path, window_size, step_size):
-        self.features, self.targets = load_datalist(path)
+    def __init__(self, list_path, window_size, step_size):
+        with open(list_path) as f:
+            data_list = [s.strip().split(',')[0] for s in f.readlines() if s[0] != '#']
+        root_dir = osp.dirname(list_path)
+
+        self.features, self.targets = load_datalist(root_dir, data_list)
         self.sample_ids = []
         for i, feat in enumerate(self.features):
             sample_pt = np.arange(window_size / 2, feat.shape[0], step_size, dtype=np.int)[:, np.newaxis]
@@ -133,6 +143,24 @@ class IMUMagnetDataset(Dataset):
         return self.sample_ids.shape[0]
 
 
+class IMUMagnetSeqDataset(Dataset):
+    def __init__(self, list_path):
+        with open(list_path) as f:
+            data_list = [s.strip().split(',')[0] for s in f.readlines() if s[0] != '#']
+        root_dir = osp.dirname(list_path)
+        self.features, self.targets = load_datalist(root_dir, data_list)
+        self.num_feature_channel = self.features[0].shape[1]
+        self.num_target_channel = self.targets[0].shape[1]
+
+    def __getitem__(self, item):
+        feat = self.features[item].astype(np.float32).T
+        label = self.targets[item].astype(np.float32).T
+        return feat, label
+
+    def __len__(self):
+        return len(self.features)
+
+
 class MagnetFusionNetwork(torch.nn.Module):
     def __init__(self, input_channel, output_channel, kernel_size, layer_channels, dropout=0.2):
         super(MagnetFusionNetwork, self).__init__()
@@ -150,6 +178,12 @@ class MagnetFusionNetwork(torch.nn.Module):
         self.output_layer.bias.data.normal_(0, 0.001)
 
 
+def write_config(args):
+    if args.out_dir:
+        with open(osp.join(args.out_dir, 'config.json'), 'w') as f:
+            json.dump(vars(args), f)
+
+
 def train(args):
     if args.out_dir:
         if not osp.isdir(args.out_dir):
@@ -158,16 +192,15 @@ def train(args):
             os.makedirs(osp.join(args.out_dir, 'checkpoints'))
         if not osp.isdir(osp.join(args.out_dir, 'logs')):
             os.makedirs(osp.join(args.out_dir, 'logs'))
-        if osp.exists(osp.join(args.out_dir, 'logs', 'log.txt')):
-            os.remove(osp.join(args.out_dir, 'logs', 'log.txt'))
+        write_config(args)
 
     train_dataset = IMUMagnetDataset(args.train_list, args.window_size, args.step_size)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_worker)
 
     val_dataset, val_loader = None, None
     if args.val_list:
-        val_dataset = IMUMagnetDataset(args.val_list, args.window_size, args.step_size)
-        val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_worker)
+        val_dataset = IMUMagnetSeqDataset(args.val_list)
+        val_loader = DataLoader(val_dataset)
 
     device = torch.device('cuda:0' if torch.cuda.is_available() and not args.cpu else 'cpu')
 
@@ -180,15 +213,18 @@ def train(args):
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(network.parameters(), args.lr)
 
+    if args.out_dir and osp.exists(osp.join(args.out_dir, 'logs', 'log.txt')):
+        os.remove(osp.join(args.out_dir, 'logs', 'log.txt'))
+
     step = 0
     best_val_loss = np.inf
+    log_file = None
+    if args.out_dir:
+        log_file = osp.join(args.out_dir, 'logs', 'log.txt')
+    valid_seq_len = args.window_size * 3 // 4
     try:
-        log_file = None
-        if args.out_dir:
-            log_file = osp.join(args.out_dir, 'logs', 'log.txt')
-
         for epoch in range(args.epochs):
-            epoch_losses = AverageMeter()
+            epoch_losses = AverageMeter(1)
             log_line = ''
             for batch_id, batch in enumerate(train_loader):
                 feature, target = batch
@@ -196,7 +232,7 @@ def train(args):
 
                 optimizer.zero_grad()
                 predicted = network(feature)
-                loss = criterion(predicted, target)
+                loss = criterion(predicted[:, -valid_seq_len:], target[:, -valid_seq_len:])
                 loss.backward()
                 optimizer.step()
                 epoch_losses.add(loss.cpu().item())
@@ -205,32 +241,30 @@ def train(args):
                     print('Initial loss %f' % (loss.cpu().item()))
                 step += 1
             print('-----------------------------')
-            print('Epoch %d, average loss: %f' % (epoch, epoch_losses.get_average()))
-            log_line += '%d %f' % (epoch, epoch_losses.get_average())
+            print('Epoch {}, average loss: {}'.format(epoch, epoch_losses.get_average()))
+            log_line += '{} {}'.format(epoch, epoch_losses.get_average())
 
             if val_loader:
-                print('Running validation...')
-
-                val_losses = AverageMeter()
+                val_losses = AverageMeter(val_dataset.num_target_channel)
                 for val_bid, val_batch in enumerate(val_loader):
                     val_feat, val_target = val_batch
-                    val_feat, val_target = val_feat.to(device), val_target.to(device)
-
                     optimizer.zero_grad()
-                    val_out = network(val_feat)
-                    val_loss = criterion(val_out, val_target)
-                    val_losses.add(val_loss.cpu().item())
-                log_line += ' %f\n' % val_losses.get_average()
-                print('Validation loss: %f' % (val_losses.get_average()))
-                if val_losses.get_average() < best_val_loss:
-                    best_val_loss = val_losses.get_average()
+                    val_out = np.squeeze(network(val_feat.to(device)).cpu().detach().numpy(), axis=0)
+                    val_target = np.squeeze(val_target.numpy(), axis=0)
+                    val_loss = np.average((val_out - val_target) * (val_out - val_target), axis=1)
+                    val_losses.add(val_loss)
+                log_line += ' {}\n'.format(val_losses.get_average())
+                avg_loss = np.average(val_losses.get_average())
+                print('Validation loss: {}/{}'.format(val_losses.get_average(), avg_loss))
+
+                if avg_loss < best_val_loss:
+                    best_val_loss = avg_loss
                     if args.out_dir:
                         model_path = osp.join(args.out_dir, 'checkpoints', 'checkpoint_%d.pt' % epoch)
                         torch.save({'model_state_dict': network.state_dict(),
                                     'epoch': epoch,
                                     'loss': epoch_losses.get_average(),
                                     'optimizer_state_dict': optimizer.state_dict()}, model_path)
-                        torch.save(network.state_dict(), model_path)
                         print('Model saved to ', model_path)
             else:
                 log_line += '\n'
@@ -274,68 +308,32 @@ def test(args):
     device = torch.device('cpu')
 
     model = MagnetFusionNetwork(_input_channel, _output_channel, args.kernel_size, _layer_channels)
-    model.load_state_dict(torch.load(args.model_path)['model_state_dict'])
+    m = torch.load(args.model_path)
+    print(m.keys())
+    model.load_state_dict(m['model_state_dict'])
     model.eval().to(device)
     print('Model %s loaded to device.' % args.model_path, device)
 
-    for data in data_list:
-        print('Loading data ', data)
-        imu_all = pandas.read_csv(osp.join(root_dir, data, 'processed/data.csv'))
-        ts = imu_all['time'].values / _nano_to_sec
-        ts -= ts[0]
+    features_all, targets_all = load_datalist(root_dir, data_list)
+    assert len(features_all) == len(targets_all)
+    for data_id in range(len(features_all)):
+        feat = features_all[data_id].astype(np.float32)
+        target = targets_all[data_id].astype(np.float32)
+        predicted = model(torch.Tensor(np.expand_dims(feat.T, axis=0))).cpu().detach().numpy().T
 
-        gyro = imu_all[['gyro_x', 'gyro_y', 'gyro_z']].values
-        linacce = imu_all[['linacce_x', 'linacce_y', 'linacce_z']].values
-        gravity = imu_all[['grav_x', 'grav_y', 'grav_z']].values
-        positions = imu_all[['pos_x', 'pos_y', 'pos_x']].values
-        magnets = imu_all[['magnet_x', 'magnet_y', 'magnet_z']].values
-        orientations = imu_all[['ori_w', 'ori_x', 'ori_y', 'ori_z']].values
-
-        # gyro_aligned = geometry.align_3dvector_with_gravity(gyro, gravity)
-        # linacce_aligned = geometry.align_3dvector_with_gravity(linacce, gravity)
-        # features = np.concatenate([gyro_aligned, linacce_aligned], axis=1)
-        # features = gaussian_filter1d(features, sigma=2.0, axis=0)
-        # features = features.astype(np.float32)
-        # targets = td.compute_local_speed_with_gravity(ts, positions, orientations, gravity)[:, [2]]
-        # targets = gaussian_filter1d(targets, sigma=30.0, axis=0)
-        # targets = targets.astype(np.float32)
-
-        features = gaussian_filter1d(imu_all[_feature_column].values, sigma=2.0, axis=0).astype(np.float32)
-        yaw_in_tango = compute_yaw_in_tango(orientations)
-        north_in_tango = compute_magnet_yaw_in_tango(orientations, magnets)
-        targets = np.mod(yaw_in_tango - yaw_in_tango[0] + north_in_tango, 2 * math.pi)[:, np.newaxis].astype(np.float32)
-
-        sample_points = np.arange(args.window_size / 2, features.shape[0], args.window_size, dtype=np.int)
-        sample_points = np.concatenate([sample_points, [features.shape[0]]], axis=0)
-
-        predicted = np.ones(targets.shape, dtype=np.float32)
-        for pt in sample_points:
-            if pt < args.window_size:
-                feat = np.zeros([args.window_size, features.shape[1]], dtype=np.float32)
-                feat[-pt:, :] = features[:pt, :]
-            else:
-                feat = features[pt - args.window_size:pt, :]
-            feat = torch.Tensor(np.expand_dims(feat.T, 0), device=device)
-            pred = model(feat).cpu().detach().numpy().squeeze(0).T
-
-            if pt < args.window_size:
-                predicted[:pt, :] = pred[-pt:, :]
-            else:
-                predicted[pt - args.window_size:pt, :] = pred
-
-        mse = (predicted - targets) * (predicted - targets)
-        mse = np.average(mse.flatten())
-        print('Prediction finished. MSE: %f' % mse)
-        plt.figure("Prediction for %s" % osp.split(data)[1])
+        mse = (predicted - target) * (predicted - target)
+        mse = np.average(mse, axis=0)
+        print('Prediction finished. MSE: %f' % np.average(mse))
+        plt.figure("Prediction for %s" % osp.split(data_list[data_id])[1])
         for i in range(predicted.shape[1]):
             plt.subplot(predicted.shape[1] * 100 + 11 + i)
-            plt.plot(ts, targets[:, i])
-            plt.plot(ts, predicted[:, i])
+            plt.plot(target[:, i])
+            plt.plot(predicted[:, i])
             plt.legend(['GT', 'Predicted'])
-        plt.text(0.5, math.pi * 2, 'MSE: %f' % mse)
+            plt.text(0.5, math.pi * 2, 'MSE: %f' % mse[i])
         plt.tight_layout()
         if args.out_dir:
-            plt.savefig(osp.join(args.out_dir, 'predicted_%s.png' % osp.split(data)[1]))
+            plt.savefig(osp.join(args.out_dir, 'predicted_%s.png' % osp.split(data_list[data_id])[1]))
             plt.close()
         else:
             plt.show()
@@ -357,7 +355,7 @@ if __name__ == '__main__':
     parser.add_argument('--step_size', type=int, default=100)
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--num_worker', type=int, default=1)
-    parser.add_argument('--kernel_size', type=int, default=5)
+    parser.add_argument('--kernel_size', type=int, default=3)
     parser.add_argument('--epochs', type=int, default=1000)
     parser.add_argument('--out_dir', type=str, default=None)
     parser.add_argument('--cpu', action='store_true')
